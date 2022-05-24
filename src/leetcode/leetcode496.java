499058e8b8fbf4d44f79e8cfd9f453322b723052
Junyan Yang <1310204834@qq.com>
2022-05-24 23:47:23 +0800

    2022.5.24

--- src/leetcode/leetcode496.java
+++ src/leetcode/leetcode496.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 public class leetcode496 {
     public static void main(String[] args) {
-        int[] nums1 = new int[]{4, 1, 2};
+        int[] nums1 = new int[]{4, 1, 2, 3};
         int[] nums2 = new int[]{1, 3, 4, 2};
         System.out.println(Arrays.toString(nextGreaterElement(nums1, nums2)));
     }
@@ -14,10 +14,10 @@ public class leetcode496 {
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> temp = new Stack<>();
         int[] res = new int[nums1.length];
-        Arrays.fill(res,-1);
+        Arrays.fill(res, -1);
         HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0 ; i< nums1.length ; i++){
-            hashMap.put(nums1[i],i);
+        for (int i = 0; i < nums1.length; i++) {
+            hashMap.put(nums1[i], i);
         }
         temp.add(0);
         for (int i = 1; i < nums2.length; i++) {
@@ -25,7 +25,7 @@ public class leetcode496 {
                 temp.add(i);
             } else {
                 while (!temp.isEmpty() && nums2[temp.peek()] < nums2[i]) {
-                    if (hashMap.containsKey(nums2[temp.peek()])){
+                    if (hashMap.containsKey(nums2[temp.peek()])) {
                         Integer index = hashMap.get(nums2[temp.peek()]);
                         res[index] = nums2[i];
                     }
