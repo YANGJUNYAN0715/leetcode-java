ac2cc1ab2e66812414bd314db636c7bf127e7e56
Junyan Yang <1310204834@qq.com>
2022-05-26 16:03:17 +0800

    2022.5.26

---
+++ src/leetcode/leetocde1047.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class leetocde1047 {
+    public static void main(String[] args) {
+        String s = "abbaca";
+        System.out.println(removeDuplicates(s));
+    }
+
+    public static String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        st.push(s.charAt(0));
+        for (int i = 1; i < n; i++) {
+            char ch = s.charAt(i);
+            if (!st.empty() && st.peek()==ch) {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!st.empty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
